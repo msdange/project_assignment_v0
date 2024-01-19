3fe06869ff902712ec339ae546d1f3f5ae9c1986
msdange <44918481+msdange@users.noreply.github.com>
2024-01-19 13:48:42 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,13 +1,25 @@
 provider "aws" {
   region = "us-east-1"
-} 
+}
 
-/*
 data "aws_ami" "latestami" {
-  owners = ["amazon"]
   most_recent = "true"
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"] # Amazon Linux 2 example
   }
-  */
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"] # For Amazon Linux AMIs
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 
 /* --------- VPC --------- */
 resource "aws_vpc" "demovpc" {
@@ -25,7 +37,7 @@ resource "aws_vpc" "demovpc" {
 
 resource "aws_subnet" "privatesubnetA" {
   vpc_id            = aws_vpc.demovpc.id
-  cidr_block        = "10.0.16.0/20" #4096 addresses
+  cidr_block        = "10.0.16.0/20"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -35,7 +47,7 @@ resource "aws_subnet" "privatesubnetA" {
 
 resource "aws_subnet" "privatesubnetB" {
   vpc_id            = aws_vpc.demovpc.id
-  cidr_block        = "10.0.32.0/20" #4096 addresses
+  cidr_block        = "10.0.32.0/20"
   availability_zone = "us-east-1b"
 
   tags = {
@@ -48,7 +60,7 @@ resource "aws_subnet" "privatesubnetB" {
 
 resource "aws_subnet" "publicsubnetA" {
   vpc_id                  = aws_vpc.demovpc.id
-  cidr_block              = "10.0.0.0/24" #256 addresses
+  cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -59,7 +71,7 @@ resource "aws_subnet" "publicsubnetA" {
 
 resource "aws_subnet" "publicsubnetB" {
   vpc_id                  = aws_vpc.demovpc.id
-  cidr_block              = "10.0.1.0/24" # 256 addresses
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
@@ -104,32 +116,6 @@ resource "aws_route_table_association" "public_rt_assocB" {
   route_table_id = aws_route_table.public_rt.id
 
 }
-
-/* ---- ENI ---- */
-/*
-resource "aws_network_interface" "publiceni" {
-  subnet_id       = aws_subnet.publicsubnet.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.LB_sg.id]
-
-  }
-
-resource "aws_network_interface" "privateeni" {
-  subnet_id       = aws_subnet.privatesubnet.id
-  private_ips     = ["10.0.2.50"]
-  security_groups = [aws_security_group.ec2_sg.id]
-
-  } */
-
-/* ---- EIP ---- */
-
-/*
-resource "aws_eip" "publiceip" {
-  #domain = "vpc"
-  network_interface = aws_network_interface.publiceni.id
-  associate_with_private_ip = "10.0.1.50"
-  depends_on = [aws_internet_gateway.igw]
-} */
 
 resource "aws_eip" "NAT_eip_A" {
   depends_on = [aws_internet_gateway.igw]
@@ -237,29 +223,32 @@ resource "aws_lb_listener" "demo_front_end" {
 /* ------- launch configuration and ASG ------- */
 
 resource "aws_launch_configuration" "demo_config" {
-  name     = "demo_config"
-  image_id = "ami-0df435f331839b2d6"
-  #image_id               = data.aws_ami.latestami.id
-  instance_type        = var.instance_type #"t2.micro"
-  key_name             = "Mrini-Nov 2023"
+  name = "demo_config"
+  #image_id = var.ami_id   #uncomment this line if you want to test for specific ami. # Ensure to update ami value in varaible file
+  image_id             = data.aws_ami.latestami.id #comment this line if using above line
+  instance_type        = var.instance_type
+  key_name             = var.instance_key_name
   security_groups      = [aws_security_group.ec2_sg.id]
   iam_instance_profile = "ec2_profile"
 
   associate_public_ip_address = false
 
-  user_data = <<-EOF
+  /*user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
     sudo yum install -y httpd
     sudo systemctl enable httpd
     sudo systemctl start httpd  
     
-  EOF
+  EOF */
+
+  user_data = filebase64("user_data.sh")
 
 
   root_block_device {
     volume_type = "gp2"
     volume_size = "20"
+    encrypted   = true
   }
 
   ebs_block_device {
@@ -267,9 +256,8 @@ resource "aws_launch_configuration" "demo_config" {
     volume_type = "gp2"
     volume_size = "52"
     iops        = "1500"
+    encrypted   = true
   }
-
-  # user_data = file("apache.sh")
 
   lifecycle {
     create_before_destroy = true
@@ -280,13 +268,11 @@ resource "aws_autoscaling_group" "demo_asg" {
   name                = "terraform-asg-demo"
   vpc_zone_identifier = [aws_subnet.privatesubnetA.id, aws_subnet.privatesubnetB.id]
   min_size            = 1
-  max_size            = 5
-  desired_capacity    = 3
+  max_size            = 3
+  desired_capacity    = 2
   force_delete        = true
 
   launch_configuration = aws_launch_configuration.demo_config.name
-
-  #count = demo_asg.desired_capacity
 
   tag {
     key                 = "Name"
@@ -303,27 +289,28 @@ resource "aws_autoscaling_group" "demo_asg" {
 
 
 resource "aws_instance" "ansible_master" {
-  ami                    = var.ami_id            # Specify your desired Amazon Linux 2 AMI
-  instance_type          = var.instance_type     # Specify your desired instance type
-  key_name               = var.instance_key_name # Specify your key pair name
+  ami                    = data.aws_ami.latestami.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_key_name
   subnet_id              = aws_subnet.publicsubnetA.id
   vpc_security_group_ids = [aws_security_group.ALB_sg.id]
   #iam_instance_profile   = "ec2_profile"
 
+  /*
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
               sudo yum install -y python3
               sudo yum install -y ansible
               EOF
+*/
+
+  user_data = filebase64("ansible_master.sh")
 
   tags = {
     Name = "ansible_master"
   }
 
-  /*provisioner "local-exec" {
-    command = "echo ${aws_instance.ansible_master.public_ip} >> /etc/hosts"
-  } */
 }
 
 
@@ -436,4 +423,23 @@ EOF
 resource "aws_iam_role_policy_attachment" "demo-ssm-policy" {
   role       = aws_iam_role.demo-iam-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+/* ---- Cloudwatch alarm ---- */
+
+resource "aws_cloudwatch_metric_alarm" "test-alarm" {
+  alarm_name          = "test-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 0.15
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.demo_asg.name
+  }
+
+  alarm_description = "This metric monitors ec2 cpu utilization"
 }
