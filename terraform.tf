26fdc12b3251cd3b9b2fecf6be75cea739791b7a
msdange <44918481+msdange@users.noreply.github.com>
2024-01-23 14:25:51 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,10 +7,12 @@ terraform {
   }
   required_version = "~> 1.3"
 
+/* Uncomment below mentioned block if want to enable backend */
+/*
   backend "s3" {
     key    = "demo-tfe-backend" #replace with your S3 bucket if want to enable bckend
     bucket = "demo-tfe-backend" #replace with your S3 bucket if want to enable bckend
     region = "us-east-1"        #add region of your choice
   }
 }
-
+*/
